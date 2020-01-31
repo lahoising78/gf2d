d548fddb91efb9f85843f041d89d91a0997ed0be
Luis Chiang <lhc4@njit.edu>
2020-01-31 10:20:05 -0500

    can load, free, add and remove entities

--- src/application/main.c
+++ src/application/main.c
@@ -14,28 +14,16 @@ void gf2d_config()
     gf2d_main_set_window_name( "Punti Jordan - El Sayayin de la Patria" );
 }
 
-extern float frameTime;
-void update_that_thing( Entity *e )
+void hola()
 {
-    if(!e) return;
-    if( gf2d_input_is_key_pressed( SDL_SCANCODE_D ) )
-    {
-        e->position.x += 1;
-    }
-}
-
-void un_awake_ahi()
-{
-    Entity *e = NULL;
-    e = gf2d_entity_new();
-    e->render_ent->sprite = gf2d_sprite_load_all("images/space_bug.png", 128, 128, 16);
-    e->update = update_that_thing;
-    gf2d_scene_add_entity(e);
-    gf2d_scene_remove_entity(e);
+    Entity *pe = NULL;
+    pe = gf2d_entity_new();
+    pe->render_ent->sprite = gf2d_sprite_load_image("images/backgrounds/bg_flat.png");
+    gf2d_scene_add_entity(pe);
 }
 
 void gf2d_main()
 {
     slog("MAIN");
-    gf2d_scene_load(8, 8, un_awake_ahi );
+    gf2d_scene_load(8, 8, hola );
 }
