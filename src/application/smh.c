e27b223deb8bd39c9ec8b8ebfde83e163a64700a
Luis Chiang <lhc4@njit.edu>
2020-02-19 13:10:52 -0500

    progress bar example

--- src/application/smh.c
+++ src/application/smh.c
@@ -9,14 +9,24 @@
 #include "gf2d_camera.h"
 #include "gf2d_ui.h"
 
+UIComponent *ui = NULL;
+
+void somethink(Entity *self)
+{
+    gf2d_progress_bar_set_value(ui->component.pbar, ui->component.pbar->_value - 0.1f);
+}
+
 void smh_awake()
 {
-    UIComponent *ui = NULL;
+    Entity *ent = NULL;
     Vector4D bg = {0.0f, 0.0f, 255.0f, 255.0f};
 
     ui = gf2d_ui_progress_bar_new(&bg, vector2d(100, 100), NULL, vector2d(50, 50));
     gf2d_progress_bar_set_position_and_offset(ui->component.pbar, vector2d(550.0f, 310.0f), vector2d(25.0f, 25.0f));
     gf2d_progress_bar_set_max_value(ui->component.pbar, 100.0f);
-    gf2d_progress_bar_set_value(ui->component.pbar, 10.0f);
+    gf2d_progress_bar_set_value(ui->component.pbar, 100.0f);
     gf2d_scene_add_to_drawables(ui, DET_UI);
+
+    ent = gf2d_entity_new();
+    ent->update = somethink;
 }
