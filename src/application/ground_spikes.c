e610ac91de1fd6e5a4031fe6928e462b0b720b9e
Luis Chiang <lhc4@njit.edu>
2020-03-16 19:22:50 -0400

    ground spike cooldown

--- src/application/ground_spikes.c
+++ src/application/ground_spikes.c
@@ -1,4 +1,5 @@
 #include "simple_logger.h"
+#include "game_object.h"
 #include "ground_spikes.h"
 
 typedef struct
@@ -7,14 +8,23 @@ typedef struct
     uint8_t         canCollide;
     uint8_t         useGravity;
     CollisionShape  shape;
+
+    /* dmg */
+    float dmgCooldown;
+
+    PhysicsEntity *player;
 } GroundSpikesConfig;
 
 static GroundSpikesConfig config = {0};
+
+void ground_spikes_update(Entity *self);
+void ground_spikes_touch(Entity *self, Entity *other);
 
 void ground_spikes_config(const char *filepath)
 {
     SJson *json = NULL;
     PhysicsEntity *phys = NULL;
+    SJson *obj = NULL;
 
     json = sj_load(filepath);
     if(!json)
@@ -35,6 +45,14 @@ void ground_spikes_config(const char *filepath)
     config.useGravity = phys->useGravity;
     config.shape = phys->modelBox;
     slog("%u %u %u %.2f", config.sprite, config.canCollide, config.useGravity, config.shape.position.x);
+    
+    obj = sj_object_get_value(json, "damage");
+    if(obj)
+    {
+        sj_get_float_value( sj_object_get_value(obj, "cooldown"), &config.dmgCooldown );
+    }
+
+    config.player = gf2d_physics_entity_get_by_name("punti");
 
     sj_free(json);
 }
@@ -51,6 +69,8 @@ PhysicsEntity *ground_spikes_new()
 
 void ground_spikes_init(PhysicsEntity *self)
 {
+    GameObject *gobj = NULL;
+
     if(!self) return;
     slog("init ground spike");
 
@@ -58,4 +78,42 @@ void ground_spikes_init(PhysicsEntity *self)
     self->canCollide = config.canCollide;
     self->modelBox = config.shape;
     self->useGravity = config.useGravity;
+
+    gobj = game_object_new();
+    if(gobj)
+    {
+        gobj->selfPhys = self;
+        gobj->self = self->entity;
+        self->entity->abstraction = gobj;
+    }
+
+    self->entity->update = ground_spikes_update;
+    self->entity->touch = ground_spikes_touch;
+}
+
+extern float frameTime;
+void ground_spikes_update(Entity *self)
+{
+    GameObject *gobj = NULL;
+    if(!self) return;
+
+    gobj = (GameObject*)self->abstraction;
+    if(!gobj) return;
+
+    gobj->coolDown -= frameTime;
+    if(gobj->coolDown < 0.0f) gobj->coolDown = 0.0f;
+}
+
+void ground_spikes_touch(Entity *self, Entity *other)
+{
+    GameObject *gobj = NULL;
+    if(!config.player) return;
+    if(!self || !other) return;
+    if(other != config.player->entity) return;
+
+    gobj = (GameObject*)self->abstraction;
+    if(!gobj || gobj->coolDown > 0.0f) return;
+
+    slog("spike do dmg");
+    gobj->coolDown = config.dmgCooldown;
 }
