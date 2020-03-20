cac279d007e8455ab437db8fb0b6967399741dd9
Luis Chiang <lhc4@njit.edu>
2020-03-20 18:49:51 -0400

    confirm player can block

--- src/application/combat.c
+++ src/application/combat.c
@@ -16,7 +16,7 @@ void combat_knockback(GameObject *attacker, GameObject *target, Vector2D dir, fl
 {
     if(!target) return;
     if(!target->self) return;
-    if(target->hitstun > 0.0f) return;
+    if(target->hitstun > 0.0f || target->isProtected) return;
 
     vector2d_set_magnitude(&dir, knockback);
     vector2d_copy(target->self->velocity, dir);
