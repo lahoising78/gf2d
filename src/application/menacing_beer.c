24d5f0a3885283450b4561034e48d9710b9afa65
Luis Chiang <lhc4@njit.edu>
2020-03-21 19:14:29 -0400

    beer can change states

--- src/application/menacing_beer.c
+++ src/application/menacing_beer.c
@@ -1,4 +1,5 @@
 #include "simple_logger.h"
+#include "combat.h"
 #include "game_object.h"
 #include "menacing_beer.h"
 
@@ -8,6 +9,17 @@ typedef struct
 } MenacingBeerConfig;
 
 static MenacingBeerConfig config = {0};
+
+typedef enum
+{
+    BEER_IDLE = 0,
+    BEER_ATTACKING,
+    BEER_RESTING
+} MenacingBeerState;
+
+void menacing_beer_think(Entity *self);
+void menacing_beer_update(Entity *self);
+void menacing_beer_damage(GameObject *self, GameObject *other);
 
 void menacing_beer_config(const char *filepath)
 {
@@ -19,4 +31,73 @@ void menacing_beer_config(const char *filepath)
 void menacing_beer_init(PhysicsEntity *phys)
 {
     ENEMY_INIT_BEGIN(config, phys)
+
+    if(gobj)
+    {
+        gobj->damage = menacing_beer_damage;
+    }
+
+    phys->entity->update = menacing_beer_update;
+    phys->entity->think = menacing_beer_think;
+}
+
+extern float frameTime;
+void menacing_beer_think(Entity *self)
+{
+    GameObject *gobj = NULL;
+    if(!self) return;
+    gobj = (GameObject*)self->abstraction;
+    if(!gobj) return;
+
+    if( gobj->state != BEER_RESTING )
+    {
+        if( game_object_player_in_area(gobj) )
+        {
+            gobj->state = BEER_ATTACKING;
+        }
+        else
+        {
+            gobj->state = BEER_IDLE;
+        }
+    }
+}
+
+void menacing_beer_update(Entity *self)
+{
+    GameObject *gobj = NULL;
+    if(!self) return;
+    gobj = (GameObject*)self->abstraction;
+    if(!gobj) return;
+
+    game_object_update(gobj);
+
+    switch (gobj->state)
+    {
+    case BEER_RESTING:
+        gobj->coolDown -= frameTime;
+        if(gobj->coolDown <= 0.0f)
+        {
+            gobj->coolDown = 0.0f;
+            gobj->state = BEER_IDLE;
+        }
+        break;
+    
+    default:
+        break;
+    }
+
+    slog("beer is in state %d", gobj->state);
+}
+
+void menacing_beer_damage(GameObject *self, GameObject *other)
+{
+    if(!self || !other) return;
+    if(other->selfPhys != config.player) return;
+
+    if( combat_do_damage(self, other, config.damage, config.hitstun) )
+    {
+        self->state = BEER_RESTING;
+        self->coolDown = config.cooldown;
+        vector2d_clear(self->self->velocity);
+    }
 }
